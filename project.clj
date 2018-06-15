045358d66c7b415cf6fea192d66d325739a59a59
Simon Skorokhodov <skorohodovsemen@gmail.com>
2018-06-15 17:04:26 +0200

    update dependencies

--- project.clj
+++ project.clj
@@ -6,9 +6,9 @@
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.match "0.3.0-alpha4"]
+                 [org.clojure/core.match "0.3.0-alpha5"]
                  [otplike "0.4.0-alpha"]
-                 [http-kit "2.2.0"]
+                 [http-kit "2.3.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.cognitect/transit-clj "0.8.309"]]
 
