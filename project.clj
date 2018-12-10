489bd1423407081b646835b2ed74048b1939db3f
Simon Skorokhodov <skorohodovsemen@gmail.com>
2018-12-10 19:51:01 +0100

    update dependencies

--- project.clj
+++ project.clj
@@ -4,12 +4,12 @@
   :license {:name "Eclipse Public License - v1.0"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/core.match "0.3.0-alpha5"]
                  [otplike "0.5.0-alpha"]
                  [http-kit "2.3.0"]
                  [com.taoensso/timbre "4.10.0"]
-                 [com.cognitect/transit-clj "0.8.309"]]
+                 [com.cognitect/transit-clj "0.8.313"]]
 
   :source-paths  ["src"]
 
