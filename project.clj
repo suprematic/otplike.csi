2042712ccaa9d8218db859ffb04d9d7bf0fa447e
Simon Skorokhodov <skorohodovsemen@gmail.com>
2020-09-26 14:27:42 +0200

    dependencies: update clojure, core.match and otplike

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject otplike/otplike.csi "0.3.1-SNAPSHOT"
+(defproject otplike/otplike.csi "0.4.0-SNAPSHOT"
   :description "CSI backend for otplike"
   :url "https://github.com/suprematic/otplike.csi"
   :license {:name "Eclipse Public License - v1.0"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/core.match "0.3.0-alpha5"]
-                 [otplike "0.5.0-alpha"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.match "1.0.0"]
+                 [otplike "0.6.0-alpha"]
                  [http-kit "2.3.0"]
                  [com.taoensso/timbre "4.10.0"]
                  [com.cognitect/transit-clj "0.8.313"]])
